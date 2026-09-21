f42769b11d631629099ef04c50d0652ec2d1c679
cantikaayulestari123@gmail.com <cantikaayulestari123@gmail.com>
2026-09-21 11:25:58 +0700

    inisialisasi

---
+++ meeting2.py
@@ -0,0 +1,12 @@
+nilai = int(input("masukan nilai: "))
+
+if nilai >= 90:
+    print ("exellent")
+elif nilai >= 80:
+    print("very good")
+elif nilai >= 70:
+    print("good")
+elif nilai >= 60:
+    print("avrage")
+else:
+    print("poor")
